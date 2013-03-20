4fa2d395d38c625b168ff5f294625b977be1d4af
Hans-Jakob Bollinger <contact@webbar.ch>
2013-03-20 21:45:09 +0100

    typo

--- pyroclass.php
+++ pyroclass.php
@@ -6,34 +6,34 @@
  * <html class="{{ pyroclass:classes }}"> or <body class="{{ pyroclass:classes }}">
  *
  * @author		webbear.ch
- * 
+ *
  * @copyright	Copyright (c) 2012 - 2014, webbbear.ch
  */
 class Plugin_Pyroclass extends Plugin
 {
 	/**
 	 * classes - getting the uri segments and user agents and  return them as css classes
-	 * 
+	 *
 	 * @return string
 	 */
 	function classes()
 	{
 		$body_class ='';
-		
+
 		$classes = array();
 		$segments = $this->uri->total_segments();
 		$is_home = '';
 
-		// no segment = it is the homepage 
-		if ($this->uri->segment(1) === FALSE) {
+		// no segment = it is the homepage
+		if ($this->uri->segment(1) == FALSE) {
 			$is_home = 'homepage';
 			$body_class .= $is_home;
-		} 
+		}
 		else
 		{
 			$segs = array();
 			$segs = $this->uri->segment_array();
-			for ($i = 1; $i <= $segments; $i++) 
+			for ($i = 1; $i <= $segments; $i++)
 			{
 				$seg = $this->uri->segment($i);
 				// if there is a number add a n to make css safe
@@ -43,18 +43,18 @@ class Plugin_Pyroclass extends Plugin
 			$output_class = implode(' ',$classes);
 			$body_class .= $output_class;
 		}
-		
+
 		// no let's check the user agent
 		$version =  str_replace('.','-',$this->agent->version());
 		$version_small = strstr($this->agent->version(), '.', true);
 		$browser = strtolower($this->agent->browser());
 		$ua_class = '';
 
-		if ($browser == "safari") 
+		if ($browser == "safari")
 		{
 			 $ua_class='safari' . ' safari'.$version_small;
 		}
-		elseif ($browser == "internet explorer") 
+		elseif ($browser == "internet explorer")
 		{
 
 			$ua_class = 'ie ' . 'ie'.$version .' ie'.$version_small;
@@ -80,9 +80,9 @@ class Plugin_Pyroclass extends Plugin
 		}
 		// classe glued togehter
 		$body_class .= ' '.$ua_class;
-		
+
 		return $body_class;
-		
+
 	}
 }
 
